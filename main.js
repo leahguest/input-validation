fe93359f78077df5aff8c10b16e88bffcd8bceb7
Leah Guest <leah.w.guest@vanderbilt.edu>
2016-07-01 11:58:59 -0400

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 (function () {
     var nameEl = document.querySelector('#name');
+    var description = document.querySelector('#description');
+    var hourEl = document.querySelector('#hours');
+    var selEl = document.querySelector('#skill');
+    var ingredients = document.querySelector('#ingredients');
+    var stepsEl = document.querySelector('#steps');
     var backButton = document.querySelector('#back');
     var nextButton = document.querySelector('#next');
     var steps = document.querySelectorAll('.step');
@@ -12,13 +17,22 @@
     // otherwise hide it. We call this function at the bottom of the scope to do
     // an initial show/hide.
     function updateButtons() {
-
+        if (currentStep > 0) {
+            backButton.classList.add('is-active');
+        } else {
+            backButton.classList.remove('is-active');
+        }
+        if (currentStep < steps.length - 1) {
+            nextButton.classList.add('is-active');
+        } else {
+            nextButton.classList.remove('is-active');
+        }
     }
 
     // `hideCurrentStep` should hide the step that is currently visible using
     // the `currentStep` variable.
     function hideCurrentStep() {
-
+        steps[currentStep].classList.remove('is-active');
     }
 
     // `next` should validate the inputs on the current step by calling
@@ -28,8 +42,15 @@
     // the next step should be shown (use the class `is-active`).
     // * `next` should also call `updateButtons` to display the appropriate
     // buttons.
-    function next() {
-
+    function next(e) {
+        e.preventDefault();
+        if (validate()) {
+          hideCurrentStep();
+          currentStep++;  
+          steps[currentStep].classList.add('is-active');
+          updateButtons();
+        }
+        
     }
 
     // `back` should validate the inputs on the current step by calling
@@ -39,8 +60,14 @@
     // the previous step should be shown (use the class `is-active`).
     // * `back` should also call `updateButtons` to display the appropriate
     // buttons.
-    function back() {
-
+    function back(e) {
+        e.preventDefault();
+        if (validate()) {
+            hideCurrentStep();
+            currentStep--;
+            steps[currentStep].classList.add('is-active');
+            updateButtons();
+        }
     }
 
     // Validate should evaluate the input of the inputs in the current step.
@@ -56,16 +83,75 @@
         var valid = true;
 
         if (currentStep === 0) {
+            if (nameEl.value.length < 3 || nameEl.value.length > 50) {
+                nameEl.parentElement.dataset.error = 'Name must be at least 3 characters long and no greater than 50 characters.';
+                nameEl.parentElement.classList.add('is-error');
+                valid = false;
+            }
+
             if (nameEl.value.length === 0) {
                 nameEl.parentElement.dataset.error = 'Required';
                 nameEl.parentElement.classList.add('is-error');
                 valid = false;
             }
-            /* etc. */
+
+            if (description.value.length < 20 || description.value.length > 255) {
+                description.parentElement.dataset.error = 'Name must be at least 20 characters long and no greater than 255 characters.';
+                description.parentElement.classList.add('is-error');
+                valid = false;
+            }       
+
+            if (description.value.length === 0) {
+                description.parentElement.dataset.error = 'Required';
+                description.parentElement.classList.add('is-error');
+                valid = false;                
+            }       
+
         } else if (currentStep === 1) {
+            if (hourEl.value < 0) {
+                hourEl.parentElement.dataset.error = 'You cannot have negative number of hours';
+                hourEl.parentElement.classList.add('is-error');
+                valid = false;
+            }
 
-        } /* etc. */
+            if (hourEl.value.length === 0) {
+                hourEl.parentElement.dataset.error = 'Required.';
+                hourEl.parentElement.classList.add('is-error');
+                valid = false;
+            }
 
+            if (selEl.options.selectedIndex === 0) {
+                selEl.parentElement.dataset.error = 'Please select a skill level';
+                selEl.parentElement.classList.add('is-error');
+                valid = false;
+            }
+
+        } else if (currentStep === 2) {
+            if (ingredients.value.length > 255) {
+                ingredients.parentElement.dataset.error = 'You cannot have more than 255 characters.';
+                ingredients.parentElement.classList.add('is-error');
+                valid = false;
+            }
+
+            if (ingredients.value.length === 0) {
+                ingredients.parentElement.dataset.error = 'Required';
+                ingredients.parentElement.classList.add('is-error');
+                valid = false;                 
+            }
+
+        } else if (currentStep === 3) {
+            if (stepsEl.value.length > 255) {
+                stepsEl.parentElement.dataset.error = 'You cannot have more than 255 characters.';
+                stepsEl.parentElement.classList.add('is-error');
+                valid = false;
+            }
+
+            if (stepsEl.value.length === 0) {
+                stepsEl.parentElement.dataset.error = 'Required';
+                stepsEl.parentElement.classList.add('is-error');
+                valid = false;                 
+            }
+        }
         return valid;
     }
 
@@ -73,11 +159,17 @@
     // The event handler should prevent any default functionality the
     // browser has for `<button>` elements and call the `next` function. 
 
+    nextButton.addEventListener('click', next);
+
     // Register an event handler for the 'click' event on the back button.
     // The event handler should prevent any default functionality the
     // browser has for `<button>` elements and call the `back` function. 
 
+    backButton.addEventListener('click', back);
+
     // Call `updateButtons` initially to make sure the proper buttons are
     // visible.
     updateButtons();
+
+
 })();
